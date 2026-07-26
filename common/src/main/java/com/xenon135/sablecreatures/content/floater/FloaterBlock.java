92226408c4e95a6ee5e31e554ba109aa0ebb629f
solveforx <ironmuscle475@gmail.com>
2026-07-26 13:53:35 -0400

    europe trip megacommit, added everything

--- common/src/main/java/com/xenon135/sablecreatures/content/floater/FloaterBlock.java
+++ common/src/main/java/com/xenon135/sablecreatures/content/floater/FloaterBlock.java
@@ -1,14 +1,21 @@
 package com.xenon135.sablecreatures.content.floater;
 
 import com.mojang.serialization.MapCodec;
+import com.xenon135.sablecreatures.CommonClass;
+import com.xenon135.sablecreatures.Constants;
+import com.xenon135.sablecreatures.index.CreaturesBlockEntities;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
-public class FloaterBlock extends BaseEntityBlock {
+public class FloaterBlock extends BaseEntityBlock implements EntityBlock {
     public FloaterBlock(Properties properties) {
         super(properties);
     }
@@ -26,5 +33,10 @@ public class FloaterBlock extends BaseEntityBlock {
     @Override
     public @Nullable BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
         return new FloaterBlockEntity(blockPos, blockState);
+    }
+
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> blockEntityType) {
+        return createTickerHelper(blockEntityType, CreaturesBlockEntities.FLOATER.get(), FloaterBlockEntity::tick);
     }
 }
--- common/src/main/java/com/xenon135/sablecreatures/content/floater/FloaterBlockEntity.java
+++ common/src/main/java/com/xenon135/sablecreatures/content/floater/FloaterBlockEntity.java
@@ -1,7 +1,5 @@
 package com.xenon135.sablecreatures.content.floater;
 
-import com.xenon135.sablecreatures.CommonClass;
-import com.xenon135.sablecreatures.Constants;
 import com.xenon135.sablecreatures.index.CreaturesBlockEntities;
 import dev.ryanhcode.sable.api.block.BlockEntitySubLevelActor;
 import dev.ryanhcode.sable.api.physics.force.ForceGroups;
@@ -9,15 +7,27 @@ import dev.ryanhcode.sable.api.physics.force.QueuedForceGroup;
 import dev.ryanhcode.sable.api.physics.handle.RigidBodyHandle;
 import dev.ryanhcode.sable.companion.math.JOMLConversion;
 import dev.ryanhcode.sable.sublevel.ServerSubLevel;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Vec3i;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.joml.Vector3d;
-import org.lwjgl.opengl.INTELConservativeRasterization;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
 
 public class FloaterBlockEntity extends BlockEntity implements BlockEntitySubLevelActor {
 
-
+    Vector3d targetPos = new Vector3d(0,20,0);
+    Vector3d localPos = new Vector3d();
+    Vector3d globalPos = new Vector3d();
+    Vector3d force = new Vector3d();
+    double P = 0.5;
+    BlockPos targetBlock = null;
 
 
     public FloaterBlockEntity(BlockPos pos, BlockState state){
@@ -27,7 +37,50 @@ public class FloaterBlockEntity extends BlockEntity implements BlockEntitySubLev
     @Override
     public void sable$physicsTick(ServerSubLevel subLevel, RigidBodyHandle handle, double timeStep) {
         final QueuedForceGroup forceGroup = subLevel.getOrCreateQueuedForceGroup(ForceGroups.PROPULSION.get());
-        Vector3d thrustPos = JOMLConversion.toJOML(this.getBlockPos().getCenter());
-        forceGroup.applyAndRecordPointForce(thrustPos, new Vector3d(0, 1, 0));
+        JOMLConversion.atLowerCornerOf(this.getBlockPos(), localPos);
+        subLevel.logicalPose().transformPosition(localPos, globalPos);
+        if (targetBlock == null) { return; }
+        targetPos.sub(globalPos, force).mul(P);
+        clampVectorLength(force, 0, 1);
+        forceGroup.applyAndRecordPointForce(localPos, subLevel.logicalPose().transformNormalInverse(force));
+    }
+
+    public void clampVectorLength(Vector3d vector, double lengthMin, double lengthMax){
+        double clampedLength = Math.clamp(vector.length(), lengthMin, lengthMax);
+        vector.div(vector.length()).mul(clampedLength);
+    }
+
+    public static void tick(Level level, BlockPos pos, BlockState state, FloaterBlockEntity blockEntity){
+        if (level instanceof ClientLevel) return;
+        blockEntity.tickBE(level);
+    }
+
+    private void tickBE(Level level){
+        BlockPos pos = new BlockPos((int) globalPos.x, (int) globalPos.y, (int) globalPos.z);
+        targetBlock = findNearestBlockInstance(pos, 10, Blocks.BAMBOO, level);
+        if (targetBlock != null){
+            JOMLConversion.atCenterOf(targetBlock, targetPos);
+        }
+    }
+
+    @Nullable
+    //if null, no instance found
+    private BlockPos findNearestBlockInstance(BlockPos pos, int cubeSize, Block block, Level level){
+        BlockPos corner1 = pos.offset(cubeSize, cubeSize, cubeSize);
+        BlockPos corner2 = pos.offset(-cubeSize,-cubeSize,-cubeSize);
+        double distance = cubeSize + 1;
+        BlockPos.MutableBlockPos closestPos = null;
+        Iterable<BlockPos> positions = BlockPos.betweenClosed(corner1, corner2);
+        for (BlockPos posToCheck : positions){
+            if (level.getBlockState(posToCheck).getBlock() == block) {
+                double distToCheck = JOMLConversion.atLowerCornerOf(posToCheck.subtract(pos)).length();
+                if (distance > distToCheck) {
+                    distance = distToCheck;
+                    if (closestPos == null) closestPos = new BlockPos.MutableBlockPos();
+                    closestPos.set(posToCheck);
+                }
+            }
+        }
+        return closestPos;
     }
 }
